7d571bc71ff7599d417ab381de06d18bca709cab
Charles Louw <charles.louw@gmail.com>
2017-03-28 11:12:00 +0200

    Getting project to work as npm typescript package / module

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SettingsStatic {
-    private _settings;
-    extend(key: string, ...args: any[]): void;
-    get(key: string, otherwise?: any): any;
-    set(key: string, value: any): void;
-}
-export declare const Settings: SettingsStatic;
